750c3dde53630b7a11fa8faf46e6ec2670283320
ravivasavan <ravivasavan@gmail.com>
2026-04-30 12:19:31 +1000

    chore: remove agentation and playwright dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { Agentation } from 'agentation'
 import './styles/global.css'
 import App from './App'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {import.meta.env.DEV && <Agentation />}
   </StrictMode>,
 )
